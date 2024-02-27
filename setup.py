969381742ec502d3b5ee22000804a69b54022cbf
AlbertEngstfeld <albert.engstfeld@gmail.com>
2024-02-27 22:10:53 +0100

    Remove typoes and adapt readme

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 # ********************************************************************
 #  This file is part of electrochemistry-data.
 #
-#        Copyright (C)      2024 Albert Engstfeld
+#        Copyright (C) 2024 Albert Engstfeld
 #
-#  echemdb is free software: you can redistribute it and/or modify
+#  electrochemistry-data is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  echemdb is distributed in the hope that it will be useful,
+#  electrochemistry-data is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with echemdb. If not, see <https://www.gnu.org/licenses/>.
+#  along with electrochemistry-data. If not, see <https://www.gnu.org/licenses/>.
 # ********************************************************************
 
 from distutils.core import setup
@@ -22,7 +22,7 @@ from distutils.core import setup
 setup(
     name='electrochemistry_data',
     version="0.2.1",
-    packages=['echemdb_ecdata'],
+    packages=['electrochemistry-data_ecdata'],
     license='GPL 3.0+',
     long_description=open('README.md').read(),
     include_package_data=True,
